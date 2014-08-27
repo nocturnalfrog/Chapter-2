2f7ad20639f7e0dcb20a87b6eaf04ca49330acc7
Tjerk Ameel <tjerk.ameel@gmail.com>
2014-08-27 22:52:47 +0200

    So far so good

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,9 +5,64 @@ module.exports = function (grunt) {
 
     var config = {};
 
+    config['watch'] = {
+        options: {
+//            nospawn: true
+        },
+        compass: {
+            files: ['src/styles/{,*/}*.{scss,sass}'],
+            tasks: ['compass:server']
+        },
+        jade: {
+            files: ['src/templates/{,*/}*.jade'],
+            tasks: ['jade:server']
+        }
+    };
+
+    config['compass'] = {
+        options: {
+            sassDir: 'src/styles/sass',
+            cssDir: 'src/styles',
+            importPath: 'src/bower_components',
+            relativeAssets: false
+        },
+        dist: {},
+        server: {}
+    };
+
+    config['jade'] = {
+        dist: {
+            files: {
+                'src/index.html':
+                    'src/templates/index.jade',
+                'src/golden-dragon.html':
+                    'src/templates/golden-dragon.jade',
+                'src/little-pizzeria.html':
+                    'src/templates/little-pizzeria.jade'
+            }
+        },
+        server: {
+            options: {
+                data: {
+                    debug: false
+                }
+            },
+            files: {
+                'src/index.html':
+                    'src/templates/index.jade',
+                'src/golden-dragon.html':
+                    'src/templates/golden-dragon.jade',
+                'src/little-pizzeria.html':
+                    'src/templates/little-pizzeria.jade'
+            }
+        }
+    };
+
     grunt.initConfig(config);
 
-    var tasks = [];
+    var tasks = [
+        'watch'
+    ];
 
     grunt.registerTask('build', tasks);
 };
